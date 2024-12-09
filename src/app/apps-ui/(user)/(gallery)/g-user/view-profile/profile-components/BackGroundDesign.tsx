56b34158f1e3933c000ce006fa228493006478c4
Alro John <alromercado08@gmail.com>
2024-12-09 14:36:02 +0800

    asd

--- src/app/apps-ui/(user)/(gallery)/g-user/view-profile/profile-components/BackGroundDesign.tsx
+++ src/app/apps-ui/(user)/(gallery)/g-user/view-profile/profile-components/BackGroundDesign.tsx
@@ -2,7 +2,7 @@
 
 export const BackGroundDesign = () => {
   return (
-    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-new-1 via-new-4 to-new-3">
+    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-new-1 via-new-4 to-new-5">
       <div className="absolute -top-[6rem] -left-[9rem] z-[50] w-[24rem] h-[24rem] bg-new-5 rounded-full"></div>
       <div className="absolute -top-[7rem] -left-[10rem] z-[40] w-[30rem] h-[30rem] bg-new-4 rounded-full"></div>
       <div className="absolute -top-[8rem] -left-[11rem] z-[30] w-[36rem] h-[36rem] bg-new-3 rounded-full"></div>
