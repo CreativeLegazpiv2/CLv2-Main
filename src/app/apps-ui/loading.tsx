bfad54a7b2b5149ee2c21de95f37bbd7ebf915d2
Alro John <alromercado08@gmail.com>
2024-12-20 11:52:50 +0800

    asdf

--- src/app/apps-ui/loading.tsx
+++ src/app/apps-ui/loading.tsx
@@ -1,14 +1,9 @@
 import { Loader } from "lucide-react";
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <LoadingSkeleton />;
-}
-
-const LoadingSkeleton = () => {
   return (
-    <div className="bg-gray-50 text-black fixed min-h-screen h-dvh min-w-screen w-dvh flex justify-center items-center">
-       <Loader size={70} className="animate-spin" />
+    <div className="bg-gray-50 fixed min-h-screen w-full flex justify-center items-center">
+      <Loader size={70} className="animate-spin" />
     </div>
   );
-};
+}
