a22528f234011cb5db4a00b79ff87eefbbddf776
Alro John <alromercado08@gmail.com>
2025-02-04 15:07:23 +0800

    asd

--- src/app/(apps-ui)/(pages)/gallery/page.tsx
+++ src/app/(apps-ui)/(pages)/gallery/page.tsx
@@ -1,20 +1,31 @@
 import { Infinite } from "@/components/reusable-component/Infinite";
 import { Subscribe } from "@/components/reusable-component/Subscribe";
-import IndividualWorks from "./g-visitor-components/IndividualWorks";
-import StatisticsBanner from "./g-visitor-components/StatisticsBanner";
-import BrowseGallery from "./g-visitor-components/BrowseGallery";
 import GalleryTitle from "./g-visitor-components/GalleryTitle";
+import { FeaturedCollections } from "../../(user)/gallery-display/g-user-components/FeaturedGallery";
+import { CreativeDirectory } from "../home/landing-page/CreativeDirectory";
+// import IndividualWorks from "./g-visitor-components/IndividualWorks";
+// import StatisticsBanner from "./g-visitor-components/StatisticsBanner";
+// import BrowseGallery from "./g-visitor-components/BrowseGallery";
+// import GalleryTitle from "./g-visitor-components/GalleryTitle";
+
+
 
 export default function GalleryVisitorPage() {
     return (
         <main className="w-full h-fit text-primary-2 overflow-x-hidden bg-palette-5"> {/* Ensure no horizontal scrolling */}
             <div className="pt-[10dvh] flex flex-col w-full max-w-full"> {/* Ensure full width with no overflow */}
-                <GalleryTitle />
+                {/* <GalleryTitle />
                 <BrowseGallery />
                 <StatisticsBanner />
                 <IndividualWorks />
+                <Infinite /> */}
+
+                <GalleryTitle />
+                <CreativeDirectory />
+
+                <FeaturedCollections />
                 <Infinite />
-                
+
             </div>
         </main>
     )
