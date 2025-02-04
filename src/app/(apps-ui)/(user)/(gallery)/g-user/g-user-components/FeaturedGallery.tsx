50edb66a3233e088e3c83049486dcba417bdf2eb
Alro John <alromercado08@gmail.com>
2025-02-04 10:00:47 +0800

    asd

--- src/app/(apps-ui)/(user)/(gallery)/g-user/g-user-components/FeaturedGallery.tsx
+++ src/app/(apps-ui)/(user)/(gallery)/g-user/g-user-components/FeaturedGallery.tsx
@@ -74,7 +74,7 @@ export const FeaturedCollections = () => {
   );
 
   return (
-    <section className="bg-white py-20 px-4 sm:px-6 lg:px-8">
+    <section className="bg-white py-20 px-4 sm:px-6 lg:px-8 min-h-screen h-fit">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
