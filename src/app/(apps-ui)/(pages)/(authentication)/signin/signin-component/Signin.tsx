c0633f85b87da23608bf5fea9fe5c670a54a4947
Alro John <alromercado08@gmail.com>
2025-02-05 12:27:23 +0800

    asd

--- src/app/(apps-ui)/(pages)/(authentication)/signin/signin-component/Signin.tsx
+++ src/app/(apps-ui)/(pages)/(authentication)/signin/signin-component/Signin.tsx
@@ -11,6 +11,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Form } from "./SiginForm";
 import { ForgotPass } from "./ForgotPass";
+import { X } from "lucide-react";
 
 export const Signin = () => {
   return (
@@ -19,9 +20,10 @@ export const Signin = () => {
       <div className="absolute inset-0 w-full h-full bg-black/50"></div> {/* Increased opacity for better contrast */}
 
       {/* Content */}
-      <div className="relative w-full h-full xl:max-w-[55%] sm:max-w-[70%] max-w-[95%] mx-auto flex flex-col gap-10 justify-center items-center">
+      <div className="relative w-full h-full xl:max-w-[60%] sm:max-w-[70%] max-w-[95%] mx-auto flex flex-col gap-10 justify-center items-center">
         <AccountCreation />
       </div>
+      <ToastContainer />
     </div>
   );
 };
@@ -29,6 +31,7 @@ export const Signin = () => {
 const AccountCreation = () => {
   // State to track if "Forgot password" was clicked
   const [forgotPassword, setForgotPassword] = useState(false);
+  const [showSIgnup, setShowSignup] = useState(false);
 
   // Function to toggle the forgot password state
   const handleForgotPasswordClick = () => {
@@ -42,7 +45,7 @@ const AccountCreation = () => {
 
   return (
     <div className="w-full h-full relative">
-      <ToastContainer />
+
       {/* Main content */}
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
@@ -53,13 +56,13 @@ const AccountCreation = () => {
         <div className="w-full h-full justify-center items-center sm:p-10 p-6 lg:flex hidden">
           <Logo color="text-palette-5" width={"auto"} height={"auto"} />
         </div>
-        <div className="w-full h-full flex flex-col bg-palette-5 text-palette-1 rounded-r-2xl gap-12 justify-center items-center sm:p-10 p-6">
+        <div className="w-full h-full flex flex-col bg-palette-5 text-palette-1 lg:rounded-r-2xl lg:rounded-none rounded-2xl gap-12 justify-center items-center sm:p-10">
           <div className="w-full h-fit flex flex-col gap-4 justify-end items-center">
             <div className="w-full h-fit">
               <span className="title text-3xl tracking-wider uppercase">{forgotPassword ? "Forgot" : "Login"}</span>
               <h1 className="uppercase text-5xl font-bold poppins">{forgotPassword ? "Password" : "Explore"}</h1>
             </div>
-            
+
             {/* Conditionally render either the login form or forgot password */}
             {forgotPassword ? (
               <ForgotPass handleBackToLogin={handleBackToLogin} />
@@ -68,28 +71,41 @@ const AccountCreation = () => {
             )}
 
             {/* Footer Links */}
-            {!forgotPassword && (
-              <div className="w-full flex justify-between items-center text-sm">
-                {/* Left: Signup as Buyer */}
-                <button 
-                    type="button" 
-                    className="border border-palette-1 px-2 py-1 rounded-md"
+            <div className="w-full h-fit flex flex-row gap-4 justify-between items-center text-sm max-w-[95%]">
+              {!forgotPassword && (
+                <>
+                  {/* Left: Signup as Buyer */}
+                  <span
+                    className="py-1 rounded-md whitespace-nowrap cursor-pointer"
                     onClick={handleForgotPasswordClick}
                   >
                     Forgot password?
-                  </button>
+                  </span>
 
-                {/* Right: Forgot password + Apply as Artist */}
-                <div className="flex items-center gap-x-4">
-                <Link href="/signupBuyer" className="text-palette-1 hover:underline">
-                  Signup as Buyer
-                </Link>
-                  <Link href="/applyartist" className="text-palette-1 hover:underline">
+                  {/* Right: Forgot password + Apply as Artist */}
+
+                </>
+              )}
+              {!showSIgnup && !forgotPassword && (
+                <span className="cursor-pointer" onClick={() => setShowSignup(!showSIgnup)}>Register Now</span>
+              )}
+              {showSIgnup && (
+                <>
+                  <Link href="/signupBuyer" className="text-palette-1 hover:underline whitespace-nowrap cursor-pointer">
+                    Signup as Buyer
+                  </Link>
+                  <Link href="/applyartist" className="text-palette-1 hover:underline whitespace-nowrap cursor-pointer">
                     Apply as Artist
                   </Link>
-                </div>
-              </div>
-            )}
+
+                  <span><X className="bg-palette-2 text-palette-5 rounded-md" onClick={() => setShowSignup(!showSIgnup)}/></span>
+                </>
+
+              )}
+
+
+
+            </div>
           </div>
         </div>
       </motion.div>
